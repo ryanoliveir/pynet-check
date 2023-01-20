003d49d9209ccd8b080ad9c406171b97d58acd69
Ryan Soares <ryandehallo229@gmail.com>
2023-01-20 18:11:30 -0300

    Check internet connection added (using requests)

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import requests as req
+from requests.exceptions import ConnectTimeout
 
+TIMEOUT  = 3
+site_address = "www.google.com"
+
+def checkConnection(url):
+    try:
+        req.get('https://' + url, timeout=TIMEOUT)
+        return True
+    except ConnectTimeout:
+        return False
+
+
+if __name__ == "__main__":
+
+    checkConnection(site_address)
+
+    status = "Access internet!" if checkConnection(site_address) else "No internet connection!"
+
+    print(status)
+    
